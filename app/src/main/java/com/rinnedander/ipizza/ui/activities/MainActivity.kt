5d5b02bd43c7feebdadda828857fe93596ff7302
Rinnedander <kirill_170717@list.ru>
2023-07-27 15:20:36 +0700

    feat: edit maniefst and create activity

---
+++ app/src/main/java/com/rinnedander/ipizza/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rinnedander.ipizza.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rinnedander.ipizza.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/rinnedander/ipizza/ui/app/Application.kt
@@ -0,0 +1,4 @@
+package com.rinnedander.ipizza.ui.app
+
+class Application {
+}
