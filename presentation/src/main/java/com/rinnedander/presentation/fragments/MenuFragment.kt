7b275b6174363a381aa0c279fab730b1ae340a9b
Rinnedander <kirill_170717@list.ru>
2023-08-04 12:30:17 +0700

    feat: add fragment_menu, holder_pizza, splashScreen, night theme

---
+++ presentation/src/main/java/com/rinnedander/presentation/fragments/MenuFragment.kt
@@ -0,0 +1,12 @@
+package com.rinnedander.presentation.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rinnedander.presentation.R
+
+class MenuFragment : Fragment() {
+
+}
